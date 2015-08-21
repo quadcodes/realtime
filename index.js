3cdfc829fe496d1cc156bb937f89a06fdb717930
Mandar Shirke <quadcodes@gmail.com>
2015-08-21 18:32:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ var url = require('url');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 
-// "process.env.PORT" to set port by Heroku
-var port = process.env.PORT || 8080;
 
-server.listen(port, function(){
-  console.log('listening on:' + port);
+server.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
 app.get('/submit', function(req, res){
   var data = url.parse(req.url,true).query;
   io.emit('temperature', data);
   res.send('Temperature Updated to: ' + data.temperature);
+});
+
+/*
+io.on('connection', function(socket, res) {
+  socket.on('switch', function(status) {
+    io.emit('switch', status);
+    res.send('switch :' + status);
+  });
+});
+*/
+
+io.on('connection', function(socket){
+  socket.on('switch', function(msg){
+    io.emit('switch', msg);
+    socket.send('switch :' + msg);
+    //console.log(msg);
+  });
 });
 
 app.get('/', function(req, res){
