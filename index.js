a93446d20c88837c7ce61160b34f85d523a597f7
Mandar Shirke <quadcodes@gmail.com>
2015-08-22 16:52:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var url = require('url');
-//var fs = require('fs'),
-//var path = require('path'),
+var fs = require('fs');
+var path = require('path');
+var SerialPort = require('serialport').SerialPort;
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 
 
 // Serial Port
-var SerialPort = require('serialport').SerialPort;
+
 var serialPort = new SerialPort('/dev/cu.usbserial-A9E9H3RJ', {
-    baudrate: 115200
+    baudrate: 115200;
 });
 
 
@@ -27,7 +28,6 @@ io.on('connection', function(socket){
   socket.on('switch', function(msg){
     switch_status = msg;
     console.log(msg);
-
 	//write to serialport
   	serialPort.write(msg + "\n", function(err, results) {
         console.log('bytes written: ', results);
