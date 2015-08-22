8f9324bcf40390043d1a6f546dc123db06b62468
Mandar Shirke <quadcodes@gmail.com>
2015-08-22 16:13:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ var url = require('url');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 
+// Serial Port
+var serialport = require('serialport');
+var portName = '/dev/cu.usbserial-A9E9H3RJ';
+var sp = new serialport.SerialPort(portName, {
+    baudRate: 115200,
+    parser: serialport.parsers.readline("\n"),
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+});
+
 // "process.env.PORT" to set port by Heroku
 var port = process.env.PORT || 8080;
 
@@ -17,11 +29,17 @@ io.on('connection', function(socket){
   socket.on('switch', function(msg){
     switch_status = msg;
     console.log(msg);
+    
+		//write to serialport
+  	sp.write(msg + "\n", function(err, results) {
+        console.log('bytes written: ', results);
+    });    
+    
   });
 });
 
 app.get('/switch', function(req, res){
-    res.json({ switch: switch_status });
+  res.json({ switch: switch_status });
 });
 
 app.get('/submit', function(req, res){
@@ -31,5 +49,5 @@ app.get('/submit', function(req, res){
 });
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
